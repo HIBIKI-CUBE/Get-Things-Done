2d9401885c1b9f4108cfc175f981f9fb4a7a0f31
HIBIKI <hibiki.apple.1984@gmail.com>
2024-09-15 17:51:25 +0900

    Add: PassKeys Authentication capability with Auth.js and Prisma

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { page } from '$app/stores'
+  import { SignIn, SignOut } from '@auth/sveltekit/components'
+</script>
+
+<h1>SvelteKit Auth Example</h1>
+<div>
+  {#if $page.data.session}
+    <span class='signedInText'>
+      <small>Signed in as</small><br />
+      <strong>{$page.data.session.user?.name ?? 'User'}</strong>
+    </span>
+    <SignOut>
+      <div slot='submitButton' class='buttonPrimary'>Sign out</div>
+    </SignOut>
+  {:else}
+    <span class='notSignedInText'>You are not signed in</span>
+    <SignIn provider='passkey' />
+  {/if}
+</div>
